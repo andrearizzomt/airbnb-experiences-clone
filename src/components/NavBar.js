555671804c249823e465ba874734315bbe121a09
Andrea Rizzo <andrearizzo85@gmail.com>
2022-04-20 18:15:06 +0200

    Create App NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnblogo from "../images/airbnb.png";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <img className="nav--logo" src={airbnblogo} alt="airbnblogo" />
+    </nav>
+  );
+}
