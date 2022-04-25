a620a1a55acc253cbe5b7c6a2998e0173a97a377
Andrea Rizzo <andrearizzo85@gmail.com>
2022-04-25 17:54:05 +0200

    Pass data object as props - Less code and cleaner

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ export default function App() {
     return (
       <Card
         key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+        // passing the whole object (item) as props
+        item={item}
+        // img={item.coverImg}
+        // rating={item.stats.rating}
+        // reviewCount={item.stats.reviewCount}
+        // location={item.location}
+        // title={item.title}
+        // price={item.price}
+        // openSpots={item.openSpots}
       />
     );
   });
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   }
 
@@ -20,18 +20,18 @@ export default function Card(props) {
 
       <img
         className="card--image"
-        src={`/images/${props.img}`}
+        src={`/images/${props.item.coverImg}`}
         alt="cardimage"
       ></img>
       <div className="card--stats">
         <img className="card--star" src="/images/rating.png" alt="rating"></img>
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.item.title}</p>
       <p className="card--price">
-        <span className="bold">From €{props.price}</span> / person
+        <span className="bold">From €{props.item.price}</span> / person
       </p>
     </div>
   );
