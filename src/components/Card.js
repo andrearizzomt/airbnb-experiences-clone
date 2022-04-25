9e9097202afda37699b697b38c36b9e37b0c013e
Andrea Rizzo <andrearizzo85@gmail.com>
2022-04-25 14:42:08 +0200

    Add badge to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,21 @@ import React from "react";
 // import starimage from "/images/rating.png";
 
 export default function Card(props) {
-  console.log(props);
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {/* Conditional rendering text hardcoded way */}
+      {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+
+      {/* Conditional rendering dynamic way using JS logic */}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+
       <img
         className="card--image"
         src={`/images/${props.img}`}
@@ -17,9 +29,9 @@ export default function Card(props) {
         <span className="gray">({props.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>
-        <span className="bold">From €{props.price}</span> / person{" "}
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="bold">From €{props.price}</span> / person
       </p>
     </div>
   );
